9caa2362f0625eb1bdfbc4c3064a6edd9e12e9f3
ihebmoujahed <mjhdiheb02@gmail.com>
2022-09-09 18:26:33 +0200

    email

--- src/components/Book/Book.vue
+++ src/components/Book/Book.vue
@@ -1,46 +1,31 @@
 <template>
-    <article v-for="elem in this.Data" :key="elem" style="border-style: double">
-      <img :src="elem.image_location" alt="Sample photo">
-      <div class="text">
-        <h3>{{elem.name_car}}</h3>
-        <p>{{elem.describe_car}}.</p>
-        <p>{{elem.price_1}}.</p>
-        <p>{{elem.price_2}}.</p>
-        <p>{{elem.price_3}}.</p>
-      </div>
-    </article>
+    
     <div class="container">
-        <form @submit.prevent="sendEmail">
-          <label>Name</label>
-          <input 
-            type="text" 
-            v-model="name"
-            name="name"
-            placeholder="Your Name"
-          >
-          <label>Email</label>
-          <input 
-            type="email" 
-            v-model="email"
-            name="email"
-            placeholder="Your Email"
-            >
-          <label>National Identity Card</label>
-          <textarea 
-            name="cart_id"
-            type="number" 
-            v-model="cart_id"
-            placeholder="Your Id">
+        <form @submit.prevent="sendEmail" v-for="elem in this.Data" :key="elem">
+            <label>Name Car</label>
+            <input type="text" v-model="name_car" name="car" placeholder="Name car"  >
+            <label> Car</label>
+            <img :src="elem.image_location" alt="Sample photo">
+            <hr>
+            <label>Price</label>
+            <select class="form-select" aria-label="Default select example"  v-model="PRICE" name="prix" placeholder="Price">
+                <option >{{ elem.price_1 }}</option>
+                <option >{{ elem.price_2 }}</option>
+                <option >{{ elem.price_3 }}</option>
+            </select>
+
+            <label>Name</label>
+            <input type="text" v-model="name" name="name" placeholder="Your Name">
+            <label>Email</label>
+            <input type="email" v-model="email" name="email" placeholder="Your Email">
+            <label>National Identity Card</label>
+            <textarea name="cart_id" type="number" v-model="cart_id" placeholder="Your Id">
         </textarea>
-        <label>Your Numero</label>
-            <textarea 
-            name="number"
-            type="number" 
-            v-model="number"
-            placeholder="number">
+            <label>Your Numero</label>
+            <textarea name="number" type="number" v-model="number" placeholder="number">
           </textarea>
-          
-          <input type="submit" value="Send">
+
+            <input type="submit" value="Send">
         </form>
     </div>
 </template>
@@ -48,89 +33,104 @@
 <script>
 import emailjs from 'emailjs-com';
 export default {
-  name: 'ContactUs',
+    name: 'ContactUs',
 
-  data() {
-    return {
-    tt:"",
-    cart_id:"",
-    Data:[],
-    name: '',
-    email: '',
-    number: '',
-    PRICE:[]
-    }
-  },
-  mounted(){
-    var pr=''
-    const items = (localStorage.getItem("car"))
-    if (items) {
-      var s = JSON.parse(items);
-      console.log(s);
-      pr=s[0].price_3
-      this.PRICE=pr
-      this.Data=s;
-    }
-},
-  methods: {
-    sendEmail(e) {
-        var em = {
-          prix:this.PRICE,
-          name: this.name,
-          email: this.email,
-          number: this.number,
-          cart_id:this.cart_id
+    data() {
+        return {
+            name_car: "",
+            cart_id: "",
+            Data: [],
+            name: '',
+            email: '',
+            number: '',
+            PRICE: ""
         }
-      try {
-        emailjs.sendForm('ihebmoujahed', 'template_4n1za82', e.target, 'Rx72Cgmpnvk8w0xkR', em)
-        console.log(em)
-      } catch (result) {
-          console.log(result)
-      }
-      // Reset form field
-      this.prix=''
-      this.name = ''
-      this.email = ''
-      this.number= ''
-      this.cart_id=''
     },
-  }
+    mounted() {
+        var pr = ''
+        var name = ""
+        const items = (localStorage.getItem("car"))
+        if (items) {
+            var s = JSON.parse(items);
+            console.log(s);
+            pr = s[0].price_3
+            this.PRICE = pr
+            name = s[0].name_car
+            this.name_car = name
+            this.Data = s;
+        }
+    },
+    methods: {
+        sendEmail(e) {
+            var em = {
+                prix: this.PRICE,
+                car: this.name_car,
+                name: this.name,
+                email: this.email,
+                number: this.number,
+                cart_id: this.cart_id
+            }
+            try {
+                emailjs.sendForm('ihebmoujahed', 'template_4n1za82', e.target, 'Rx72Cgmpnvk8w0xkR', em)
+                location.replace("http://localhost:3000/professionnels/location-voiture-longue-duree-tunisie")
+            } catch (result) {
+                console.log(result)
+            }
+            // Reset form field
+            this.prix = ''
+            this.name_car = ''
+            this.image = ''
+            this.name = ''
+            this.email = ''
+            this.number = ''
+            this.cart_id = ''
+        },
+    }
 }
 </script>
 
 <style scoped>
-* {box-sizing: border-box;}
+* {
+    box-sizing: border-box;
+}
+
 label {
-  float: left;
+    float: left;
 }
-input[type=text], [type=email], textarea {
-  width: 100%;
-  padding: 12px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-  margin-top: 6px;
-  margin-bottom: 16px;
-  resize: vertical;
+
+input[type=text],
+[type=email],
+textarea {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    margin-top: 6px;
+    margin-bottom: 16px;
+    resize: vertical;
 }
+
 input[type=submit] {
-  background-color: #4CAF50;
-  color: white;
-  padding: 12px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
+    background-color: #4CAF50;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
 }
+
 input[type=submit]:hover {
-  background-color: #b8c7b9;
+    background-color: #b8c7b9;
 }
+
 .container {
-  display: block;
-  margin:auto;
-  text-align: center;
-  border-radius: 5px;
-  background-color: #f2f2f2;
-  padding: 20px;
-  width: 50%;
+    display: block;
+    margin: auto;
+    text-align: center;
+    border-radius: 5px;
+    background-color: #f2f2f200;
+    padding: 20px;
+    width: 50%;
 }
 </style>
